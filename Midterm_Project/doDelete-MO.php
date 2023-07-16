e0d5b8783b205905ab1fe17d9c9a9836cdc3a97b
Mordecai119513 <137976298+Mordecai119513@users.noreply.github.com>
2023-07-16 15:09:56 +0800

    音樂活動(Event -Management-MO)

---
+++ Midterm_Project/doDelete-MO.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!isset($_GET["id"])) {
+   die("無法操作"); 
+}
+
+require_once("../db_connect.php");
+
+$severname = "localhost";
+$username = "admin";
+$password = "12345";
+$dbname = "my_test_db";
+
+$conn = new mysqli($severname, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("資料庫連接錯誤：" . $conn->connect_error);
+}
+
+$id = $_GET["id"];
+
+$sql = "UPDATE Event_Management_MO SET statuss = 0 WHERE id = '$id'";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: Events-list-MO.php");
+    exit();
+} else {
+    echo "刪除資料錯誤: " . $conn->error;
+}
+
+$conn->close();
+?>
